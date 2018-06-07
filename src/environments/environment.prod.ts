4c1d1810def739edeab3a16d66be362ed39267b5
sameerMalik786 <muhammadshuaib@yahoo.in>
2018-06-07 15:34:57 +0530

    commit 10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  domainUrl: 'https://mean-angular4-dashbaord.herokuapp.com/',
-  apiBaseUrl: 'https://mean-angular4-dashbaord.herokuapp.com/',
-  apiSpaBaseUrl: 'https://mean-angular4-dashbaord.herokuapp.com/',
+  domainUrl: 'https://mean-angular4-dashbaord.herokuapp.com/api/v1',
+  apiBaseUrl: 'https://mean-angular4-dashbaord.herokuapp.com/api/v1',
+  apiSpaBaseUrl: 'https://mean-angular4-dashbaord.herokuapp.com/api/v1',
 
   // PAM
   maxActiveConnections: 3,
